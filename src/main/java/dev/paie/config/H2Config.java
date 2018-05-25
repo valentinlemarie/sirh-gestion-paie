2a6a60f01332142586eaffdfb8986918fbbdb669
valentinlemarie <valentin.lemarie@wanadoo.fr>
2018-05-25 15:51:35 +0200

    test bdd

--- src/main/java/dev/paie/config/H2Config.java
+++ src/main/java/dev/paie/config/H2Config.java
@@ -4,6 +4,7 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 
@@ -12,9 +13,21 @@ public class H2Config {
 
     @Bean
     public DataSource datasource() {
-        return new EmbeddedDatabaseBuilder()
+        
+         return new EmbeddedDatabaseBuilder()
             .setType(EmbeddedDatabaseType.H2)
             .addScript("schema.sql")
             .build();
+            
+    	
+    	/*
+    	 DriverManagerDataSource dataSource = new    DriverManagerDataSource();
+    	 
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost/sirh-paie");
+        dataSource.setUsername("root");
+        dataSource.setPassword("");
+        return dataSource;
+        */
     }
 }
